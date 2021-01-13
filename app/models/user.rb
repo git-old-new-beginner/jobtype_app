3ce55eb140a96ca0da96e32597aa8cc2d53f82ee
git-old-new-beginner <xr20120816@yahoo.co.jp>
2021-01-13 11:18:46 +0900

    デバイスビュー生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true
+  validates :name_reading, presence: true
+  validates :nickname, presence: true
+  validates :profile, presence: true     
+        
 end
