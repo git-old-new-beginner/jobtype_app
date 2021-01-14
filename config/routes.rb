18a3cedf3958833fd5d6e9bbe6d857665c47d3df
git-old-new-beginner <xr20120816@yahoo.co.jp>
2021-01-14 16:34:44 +0900

    commentモデル、コントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'webtypes/index'
- root to: "webtypes#index"
- resources :webtypes, only: [:new, :create, :show, :edit, :update, :destroy]
+  root to: "webtypes#index"
+  resources :webtypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create 
+  end
 end
