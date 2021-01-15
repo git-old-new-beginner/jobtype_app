6719b58f15d511af7a8d517e711e808fd360a577
git-old-new-beginner <xr20120816@yahoo.co.jp>
2021-01-15 12:11:46 +0900

    基本実装完了

--- app/controllers/webtypes_controller.rb
+++ app/controllers/webtypes_controller.rb
@@ -1,4 +1,8 @@
 class WebtypesController < ApplicationController
+  before_action :set_webtype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
     @webtype = Webtype.includes(:user)
   end
@@ -48,5 +52,12 @@ class WebtypesController < ApplicationController
 
   def webtype_params
     params.require(:webtype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  def set_webtype
+    @webtype = Webtype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @webtype.user
   end
 end
